e34704cc82b117ad3fe935d190fb96259a12b307
Mayank Godhani <mayankgodhani222@gmail.com>
2025-07-04 15:50:18 +0530

    all is done in home

--- src/landing_page/home/Stats.js
+++ src/landing_page/home/Stats.js
@@ -7,17 +7,17 @@ function Stats() {
                 <div className="col-6">
                     <h1 className='mb-4 fs-2'>Trust with confidence</h1>
 
-                    <h2 className='mb-2'>Customer-first always</h2>
+                    <h3 className='mb-2'>Customer-first always</h3>
                     <p>That's why 1.3+ crore customers trust Zerodha with ₹3.5+ lakh crores worth of equity investments</p>
 
-                    <h2>No spam or gimmicks</h2>
+                    <h3>No spam or gimmicks</h3>
                     <p>No gimmicks, spam, "gamification", or annoying push notifications.High quality apps that you use at your pace, the way you like.</p>
 
-                    <h2>The Zerodha universe</h2>
+                    <h3>The Zerodha universe</h3>
                     <p>Not just an app, but a whole ecosystem. Our investments in 30+
             fintech startups offer you tailored services specific to your needs.</p>
 
-                    <h2>Do better with money</h2>
+                    <h3>Do better with money</h3>
                     <p>With initiatives like Nudge and Kill Switch, we don't just
             facilitate transactions, but actively help you do better with your
             money.</p>
